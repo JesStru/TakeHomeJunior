1a08044637139b8ac6af8141291c52c0faa3e684
Jesper <struck.jesper@gmail.com>
2023-07-27 14:16:17 +0200

    init change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,69 +1,58 @@
 package org.example;
 
 import java.io.*;
-import java.text.DecimalFormat;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws NullPointerException, RuntimeException {
-        System.out.println("Hello world!");
-        String COMMA_DELIMITER = ",";
+    public static void main(String[] args) {
+
+        final String COMMA_DELIMITER = ",";
         List<List<String>> records = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader("Dateien/movies.csv"))) {
             String line;
             while ((line = br.readLine()) != null) {
-                String[] values = line.split(   COMMA_DELIMITER);
-                records.add(Arrays.asList(values));
+                records.add(Arrays.asList(line.split(COMMA_DELIMITER)));
             }
 
             System.out.println("Bitte geben Sie die Filmnummer ein, zu der Sie eine Bewertung wünschen");
-            BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(System.in));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-            String name = reader.readLine();
+            String movieId = reader.readLine();
 
-            if (name != "") {
+            if (!movieId.isEmpty()) {
+                String gefundenerRecord = null;
                 for (List<String> record : records) {
-                    String gefundenerRecord = "keiner";
-                    if (record.toArray()[0].equals(name)) {
-                        System.out.println("Es wurde der Film: " + record.toArray()[1] + " ausgewählt.");
-                        gefundenerRecord = record.toArray()[0].toString();
+                    if (record.get(0).equals(movieId)) {
+                        System.out.println("Es wurde der Film: " + record.get(1) + " ausgewählt.");
+                        gefundenerRecord = record.get(0);
+                        break;
                     }
-
-            if (gefundenerRecord.equals("keiner")) {
-                // nichts zu tun
-            } else {
-                List<List<String>> records2 = new ArrayList<>();
-                try (BufferedReader br2 = new BufferedReader(new FileReader("Dateien/ratings.csv"))) {
-                    String zeile;
-                    while ((zeile = br2.readLine()) != null) {
-                        String[] values = zeile.split(  COMMA_DELIMITER);
-                        records2.add(Arrays.asList(values));
-                    }
-
-                            System.out.println(gefundenerRecord);
-                    double gesamt = 0;
-                    int iterator = 0;
-                    for (List<String> record2 : records2) {
-                        if (record2.toArray()[1].toString().equals(gefundenerRecord)) {
-                            gesamt = gesamt + Double.parseDouble(record2.toArray()[2].toString());
-                            ++iterator;
+                }
+                if (gefundenerRecord != null) {
+                    List<List<String>> records2 = new ArrayList<>();
+                    try (BufferedReader br2 = new BufferedReader(new FileReader("Dateien/ratings.csv"))) {
+                        double gesamt = 0;
+                        int iterator = 0;
+                        String zeile;
+                        while ((zeile = br2.readLine()) != null) {
+                            records2.add(Arrays.asList(zeile.split(COMMA_DELIMITER)));
                         }
+                        for (List<String> record2 : records2) {
+                            if (record2.get(1).equals(gefundenerRecord)) {
+                                gesamt = gesamt + Double.parseDouble(record2.get(2));
+                                ++iterator;
+                            }
+                        }
+                        System.out.println(String.format("Die Bewertung für den Film ist: %.2f", (gesamt / iterator)));
                     }
-                    DecimalFormat f = new DecimalFormat("#0.00");
-                    System.out.println("Die Bewertung für den Film ist: " + f.format(gesamt / iterator));
-
-                } catch (Exception e) {
-                    throw e;
                 }
             }
-                }
-            }
-        } catch (Exception e) {
-            System.out.println("No File Found");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
