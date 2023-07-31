0e3ded077107025ed4f4a33b8361844ff8c8cdb4
Jesper <struck.jesper@gmail.com>
2023-07-31 02:43:50 +0200

    changed naming and catch block

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RuntimeException {
 
         final String COMMA_DELIMITER = ",";
         List<List<String>> records = new ArrayList<>();
@@ -22,35 +22,33 @@ public class Main {
             String movieId = reader.readLine();
 
             if (!movieId.isEmpty()) {
-                String gefundenerRecord = null;
+                String foundRecord = null;
                 for (List<String> record : records) {
                     if (record.get(0).equals(movieId)) {
                         System.out.println("Es wurde der Film: " + record.get(1) + " ausgewählt.");
-                        gefundenerRecord = record.get(0);
+                        foundRecord = record.get(0);
                         break;
                     }
                 }
-                if (gefundenerRecord != null) {
+                if (foundRecord != null) {
                     List<List<String>> records2 = new ArrayList<>();
                     try (BufferedReader br2 = new BufferedReader(new FileReader("Dateien/ratings.csv"))) {
-                        double gesamt = 0;
+                        double sum = 0;
                         int iterator = 0;
-                        String zeile;
-                        while ((zeile = br2.readLine()) != null) {
-                            records2.add(Arrays.asList(zeile.split(COMMA_DELIMITER)));
+                        String line2;
+                        while ((line2 = br2.readLine()) != null) {
+                            records2.add(Arrays.asList(line2.split(COMMA_DELIMITER)));
                         }
                         for (List<String> record2 : records2) {
-                            if (record2.get(1).equals(gefundenerRecord)) {
-                                gesamt = gesamt + Double.parseDouble(record2.get(2));
+                            if (record2.get(1).equals(foundRecord)) {
+                                sum = sum + Double.parseDouble(record2.get(2));
                                 ++iterator;
                             }
                         }
-                        System.out.println(String.format("Die Bewertung für den Film ist: %.2f", (gesamt / iterator)));
+                        System.out.printf("Die Bewertung für den Film ist: %.2f", (sum / iterator));
                     }
                 }
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
